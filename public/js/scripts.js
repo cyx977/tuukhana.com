8747512fafecd267e6367c800088aacb54317073
cyx977 <cyx977@outlook.com>
2022-01-09 21:07:13 +0545

    pdf added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,54 @@
-/*!
-* Start Bootstrap - Coming Soon v6.0.5 (https://startbootstrap.com/theme/coming-soon)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+/*
+Copyright 2020 Adobe
+All Rights Reserved.
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+/* Pass the embed mode option here */
+const viewerConfig = {
+    embedMode: "LIGHT_BOX"
+};
+
+/* Wait for Adobe Document Services PDF Embed API to be ready and enable the View PDF button */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    document.getElementById("view-pdf-btn").disabled = false;
+});
+
+/* Function to render the file using PDF Embed API. */
+function previewFile()
+{
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "78fe5bb1c10341979941df8b2a640d36"
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: "../assets/pdf/leaflet.pdf",
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                header: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: "Bodea Brochure.pdf"
+        }
+    }, viewerConfig);
+};
